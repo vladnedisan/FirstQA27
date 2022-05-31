801001e6fa5f1bc70500bf30bd2b4f20be1c3d54
VLADNEDISAN <vlad.nedisan@yahoo.com>
2022-05-31 12:09:45 +0300

    Commit Homework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,97 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Q27 world");
+        // Ex 1 - Print Hello
+        System.out.println( '\n' );
+        System.out.println("Homework Lab9 -Java- Ex 1 - Print Hello");
+        System.out.println( '\n' );
+        System.out.println("Hello Q27 World");
+        System.out.println("Vlad Nedisan");
+        System.out.println( '\n' );
+        // Ex 2 - sum of two numbers
+        System.out.println("Homework Lab9 -Java- Ex 2 - Sum of 2 numbers ");
+        System.out.println( '\n' );
+    int number1=6;
+        System.out.println("First number is : 6 ");
+    double number2= 7.5;
+        System.out.println("Second number is : 7.5 ");
+        double sum= number1+number2;
+        System.out.println("The sum of the numbers is: "+sum);
+        System.out.println( '\n' );
+        // Ex 3 - dividing two numbers
+        System.out.println("Homework Lab9 -Java- Ex 3 - Result of dividing two numbers ");
+        System.out.println( '\n' );
+        int nr1=3;
+        System.out.println("First number is : 3 ");
+        double nr2= 4.5;
+        System.out.println("Second number is : 4.5 ");
+        double divide= nr2/nr1;
+        System.out.println("The result of dividing nr2 by nr 1 is: "+divide);
+        System.out.println( '\n' );
+        // Ex 4 - Printing the results of some operations
+        System.out.println("Homework Lab9 -Java- Ex 4 - Results of some operations ");
+        System.out.println( '\n' );
+        System.out.println("a. Result of : -5 + 8 * 6 ");
+
+        // a. -5 + 8 * 6
+        System.out.println( '\n' );
+        double a1=-5;
+        System.out.println("First number is : -5 ");
+        double a2=8;
+        System.out.println("Second number is : 8 ");
+        double a3=6;
+        System.out.println("Third number is : 6 ");
+        double operationa=a1 + a2 * a3;
+        System.out.println("The result of operation : -5 + 8 * 6 is: "+operationa);
+        System.out.println( '\n' );
+
+        // b. (55+9) % 9
+        System.out.println("b. Result of : (55+9) % 9 ");
+        System.out.println( '\n' );
+        double b1=55;
+        System.out.println("First number is : 55 ");
+        double b2=9;
+        System.out.println("Second number is : 9 ");
+        double b3=9;
+        System.out.println("Third number is : 9 ");
+        double operationb= (b1+b2) % b3;
+        System.out.println("The result of operation : (55+9) % 9 is: "+operationb);
+
+
+        // c. 20 + -3*5 / 8
+        System.out.println( '\n' );
+        System.out.println("c. Result of : 20 + -3*5 / 8 ");
+        System.out.println( '\n' );
+        double c1=20;
+        System.out.println("First number is : 20 ");
+        double c2=-3;
+        System.out.println("Second number is : -3 ");
+        double c3=5;
+        System.out.println("Third number is : 5 ");
+        double c4=8;
+        System.out.println("Forth number is : 8 ");
+        double operationc= c1+c2*c3/c4;
+        System.out.println("The result of operation : 20 + -3*5/8 is: "+operationc);
+        System.out.println( '\n' );
+        // d. 5 + 15 / 3 * 2 - 8 % 3
+        System.out.println("d. Result of : 5 + 15 / 3 * 2 - 8 % 3 ");
+        // c. 5 + 15 / 3 * 2 - 8 % 3
+        System.out.println( '\n' );
+        double d1=5;
+        System.out.println("First number is : 5 ");
+        double d2=15;
+        System.out.println("Second number is : 15 ");
+        double d3=3;
+        System.out.println("Third number is : 3 ");
+        double d4=2;
+        System.out.println("Forth number is : 2 ");
+        double d5=8;
+        System.out.println("Fifth number is : 8 ");
+        double d6=3;
+        System.out.println("Sixth number is : 3 ");
+        double operationd= d1 + d2 / d3 * d4 - d5  % d6;
+        System.out.println( '\n' );
+        System.out.println("The result of operation : 5 + 15 / 3 * 2 - 8 % 3  is: "+operationd);
+
+
     }}
