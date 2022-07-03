232e61a771c59c94500624532368531d81bf36d8
VLADNEDISAN <vlad.nedisan@yahoo.com>
2022-07-03 20:49:08 +0300

    Commit Homework Lab 12  Tema - Java For Loops si Jav While Loop

--- src/LogicalOperations.java
+++ src/LogicalOperations.java
@@ -1,5 +1,3 @@
-import java.sql.SQLOutput;
-
 public class LogicalOperations {
 
     public int getGreaterNumber(int first, int second) {
@@ -22,15 +20,13 @@ public class LogicalOperations {
     public String TextComparison(String textInput) {
         if (textInput.equals(" FastTrackIT ")) {
             return ("Learning text comparison");
-        } else
-            return "Got to try some more";
+        } else return "Got to try some more";
     }
 
     public String BiggerSnow(int number2) {
         if (number2 > 8 || number2 == 6) {
             return ("The amount of snow this winter was(cm):" + number2);
-        } else
-            return ("The forecast snow is(cm)" + number2);
+        } else return ("The forecast snow is(cm)" + number2);
     }
 
     public String BiggerNumber(int number3) {
@@ -74,15 +70,13 @@ public class LogicalOperations {
 
         if (num % 2 == 0) {
             return true;
-        } else
-            return false;
+        } else return false;
     }
 
     public boolean isEligibleToVote(int age) {
         if (age > 18) {
             return true;
-        } else
-            return false;
+        } else return false;
     }
 
     public int maxNumber(int num1, int num2, int num3) {
@@ -133,95 +127,225 @@ public class LogicalOperations {
                 count++;
             }
             start1++;
-
         }
         System.out.println(sum);
         return (sum / count);
     }
 
+    // For loops
 //Tema 1
-    public int countTo100() {
-        int count = 0;
-        for (int start = 21; start <= 100; start++) {
-            count++;
+    public static void countTo100(int param) {
+        for (int start = param; start <= 100; start++) {
+            System.out.println(start);
         }
-        return count;
     }
-//Tema 2
-    public int countToMinus100() {
-        int count = 0;
-        for (int start = 2; start >= -100; start--) {
-            count++;
-        }
-        return count;
-    }
-//Tema 3
-    public int countBetweenTwoNr(){
-        int count = 0;
-        int y = 9;
-        for( int x =3; x<=y; x++) {
-            count++;
-        }
-        return count;
-           }
-//Tema  4
-public int countToBiggestNr(){
-        int count = 0;
-        int x = 10;
-        int y = 7;
-        if (x<y)
-            for (int i=x;i<y;i++){
-                count++;}
-            else
-                for ( int j=y; j<x; j++){
-                    count++;}
-                return count;
-    }
-//Tema 5
-    public int evenNrTo100(){
-        int count =0;
-        for (int i = 1; i <=100; i++) {
-           if (i % 2 == 0)
-               count++;
 
+    //Tema 2
+    public static void countToMinus100(int param) {
+        for (int start = param; start >= -100; start--) {
+            System.out.println(start);
         }
-        return count;
     }
-//Tema 6
-    public int notEvemNrTo100(){
-        int count = 0;
-        for (int i = 1; i <=100; i++) {
-            if (i % 2 != 0)
-                count++;
 
+    //Tema 3
+    public static void countBetweenTwoNr(int x, int y) {
+        for (int i = x; i <= y; i++) {
+            System.out.println(i);
         }
-        return count;
     }
-//Tema 7
-    public int sumTo100(){
-        int sum = 0;
-        for (int i=1;i<=100; i++) {
-            sum=sum+i;
+
+    //Tema  4
+    public static void countToBiggestNr(int x, int y) {
+        if (x <= y) {
+            for (int i = x; i <= y; i++) {
+                System.out.println(i);
+            }
+
+        } else {
+            for (int j = y; j <= x; j++) {
+                System.out.println(j);
+            }
         }
-    return sum;
     }
-//Tema 8
-    public double sumAndAverageTema() {
-        int sum = 0;
+
+    //Tema 5
+    public static void evenNrTo100() {
+        // int count = 0;
         for (int i = 1; i <= 100; i++) {
-            sum += i;
+            if (i % 2 == 0) System.out.println(i);
         }
-        return sum / 100d;
     }
-//Tema 9
- public static void printAsterisk(){
-        for (int i=7;i>=0; i--){
-            for (int j=i;j>=0; j--)
-            System.out.print("*");
+
+    //Tema 6
+    public static void notEvemNrTo100() {
+        int count = 0;
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 != 0) System.out.println(i);
+        }
+    }
+
+    //Tema 7
+    public static void sumTo100(int param) {
+        int sum = 0;
+        for (int i = param; i <= 100; i++) {
+            sum = sum + i;
+        }
+        System.out.println(sum);
+    }
+
+    //Tema 8
+    public static void sumAndAverageTema(int param) {
+        int sum = 0;
+        int count = 0;
+        for (int i = param; i <= 100; i++) {
+            sum += i;
+            count++;
+        }
+        System.out.println(sum / count);
+    }
+
+    //Tema 9
+    public static void printAsterisk() {
+        for (int i = 7; i >= 0; i--) {
+            for (int j = i; j >= 0; j--)
+                System.out.print("*");
             System.out.println();
         }
-            }
     }
+
+    // While Loops
+// Tema 1
+    public static void countTo100WL(int start) {
+        while (start <= 100) {
+            System.out.println(start);
+            start++;
+        }
+    }
+
+    //Tema 2
+    public static void countToMinus100WL(int startw) {
+        while (startw >= -100) {
+            System.out.println(startw);
+            startw--;
+        }
+    }
+
+    //Tema 3
+    public static void countBetweenTwoNrWL(int x, int y) {
+        while (x <= y) {
+            System.out.println(x);
+            x++;
+        }
+    }
+
+    //Tema 4
+    public static void countToBiggestNrWL(int x, int y) {
+        if (x <= y) {
+            while (x <= y) {
+                System.out.println(x);
+                x++;
+            }
+        } else {
+            while (y <= x) {
+                System.out.println(y);
+                y++;
+            }
+        }
+
+    }
+
+    //Tema 5
+    public static void evenNrTo100WL() {
+        int x = 1;
+        while (x <= 100) {
+            if (x % 2 == 0) {
+                System.out.println(x);
+            }
+            x++;
+        }
+    }
+
+    //Tema 6
+    public static void notEvemNrTo100WL() {
+        int x = 1;
+        while (x <= 100) {
+            if (x % 2 != 0) {
+                System.out.println(x);
+            }
+            x++;
+        }
+    }
+
+    //Tema 7
+    public static double getAverageInIntervalWL() {
+        int sum = 0;
+        double count = 0;
+        int start = 111;
+        int finish = 8899;
+        while (start <= finish) {
+            sum = sum + start;
+            start++;
+            count++;
+        }
+        return (sum / count);
+    }
+
+    //Tema 8
+    public double divideWith7WL(double x, double y) {
+        int count = 0;
+        double sum = 0;
+        while (x <= y) {
+            if (x % 7 == 0) {
+                System.out.println(x);
+                sum = sum + x;
+                count++;
+            }
+            x++;
+        }
+        return (sum / count);
+    }
+
+    //Tema
+    public static void nrFibonacci() {
+        int num1 = 0, num2 = 1;
+        int i = 1;
+        while (i <= 20) {
+            System.out.print(num1 + " ");
+            int sum = num1 + num2;
+            num1 = num2;
+            num2 = sum;
+            i++;
+        }
+    }
+
+    public static void cozaLozaWoza() {
+        int i = 1;
+        while (i <= 110) {
+            boolean test = false;
+            if (i % 3 == 0) {
+                System.out.print("coza");
+                test = true;
+            }
+            if (i % 5 == 0) {
+                System.out.print("loza");
+                test = true;
+            }
+            if (i % 7 == 0) {
+                System.out.print("woza");
+                test = true;
+            }
+            if (!test) {
+                System.out.print(i);
+            }
+            System.out.print(" ");
+            if (i % 11 == 0) {
+                System.out.println();
+            }
+            i++;
+        }
+    }
+}
+
 
 
 
